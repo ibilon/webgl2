bce98de70b5a1078d298bbcce9daababe8356fa0
Valentin Lemière <valentin@ibilon.com>
2018-08-02 22:20:40 +0200

    Hello world sample

---
+++ sample/Main.hx
@@ -0,0 +1,16 @@
+import js.Browser;
+import js.html.CanvasElement;
+
+using js.webgl2.CanvasHelpers;
+
+class Main
+{
+	static function main ()
+	{
+		var canvas : CanvasElement = cast Browser.document.getElementById("webgl");
+		var gl = canvas.getWebGL2();
+		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+		gl.clearColor(0.9, 0.2, 0.1, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+	}
+}
